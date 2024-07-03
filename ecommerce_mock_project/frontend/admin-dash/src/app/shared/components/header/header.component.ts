7155033e2d00ef0ad960032c205d2adced83547c
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2024-07-04 01:00:06 +0200

    Added responsivnes and mobile menu

--- ecommerce_mock_project/frontend/admin-dash/src/app/shared/components/header/header.component.ts
+++ ecommerce_mock_project/frontend/admin-dash/src/app/shared/components/header/header.component.ts
@@ -7,4 +7,24 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  ngOnInit() {
+    this.addMenuToggleListener();
+  }
+
+  addMenuToggleListener() {
+    const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const closeBtn = document.getElementById('menu-close-btn');
+
+    if (menuBtn && mobileMenu && closeBtn) {
+      menuBtn.addEventListener('click', () => {
+        mobileMenu.classList.remove('hidden');
+      });
+
+      closeBtn.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+      });
+    }
+  }
+}
