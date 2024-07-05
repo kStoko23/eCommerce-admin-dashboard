36dee69ab43efa5ed65bbf3ed9b6298985bfd3bd
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2024-07-05 14:49:08 +0200

    Comments and overflow behaviour

--- ecommerce_mock_project/frontend/admin-dash/src/app/shared/components/header/header.component.ts
+++ ecommerce_mock_project/frontend/admin-dash/src/app/shared/components/header/header.component.ts
@@ -22,6 +22,7 @@ export class HeaderComponent {
   activeLinkOffset: string;
   displayBackground: boolean = true;
 
+  // Constructor to subscribe to router events and update active link background on navigation end
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -30,14 +31,19 @@ export class HeaderComponent {
     });
   }
 
+  // Lifecycle hook to set the default active link after view initialization
   ngAfterViewInit(): void {
     this.setDefaultActiveLink();
   }
 
+  // Method to toggle the mobile menu open/close state
   toggleMenu() {
     this.isOpen = !this.isOpen;
   }
 
+  /* Method to set the default active link
+   * timeout needed for it to work
+   */
   setDefaultActiveLink(): void {
     setTimeout(() => {
       const defaultLink = this.links.first.nativeElement;
@@ -45,6 +51,9 @@ export class HeaderComponent {
     }, 50);
   }
 
+  /* Method to set the active link based on a mouse event
+   * (clicking one of the <a></a>)
+   */
   setActiveLink(event: MouseEvent): void {
     const target = event.target as HTMLElement;
     const linkElement = target.closest('.link');
@@ -61,11 +70,14 @@ export class HeaderComponent {
     this.updateActiveLink(link);
   }
 
+  // Method to update the active link's width and offset, and display the background
   updateActiveLink(linkElement: HTMLElement): void {
     this.activeLinkWidth = `${linkElement.offsetWidth}px`;
     this.activeLinkOffset = `${linkElement.offsetLeft}px`;
     this.displayBackground = true;
   }
+
+  // Method to update the active link background based on the current URL
   updateActiveLinkBackground(url: string): void {
     const activeLinks = this.links.filter(
       (link) => link.nativeElement.getAttribute('routerLink') === url
