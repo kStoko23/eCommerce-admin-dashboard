113c7e197367c4468a1c6c491a51a5e3d7d16125
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2024-07-05 14:49:51 +0200

    Added initial responsive-ish dashboard layout

--- ecommerce_mock_project/frontend/admin-dash/src/app/pages/page-dashboard/page-dashboard.component.ts
+++ ecommerce_mock_project/frontend/admin-dash/src/app/pages/page-dashboard/page-dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './page-dashboard.component.html',
-  styleUrl: './page-dashboard.component.css'
+  styleUrl: './page-dashboard.component.css',
 })
-export class PageDashboardComponent {
-
-}
+export class PageDashboardComponent {}
