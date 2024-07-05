4613c76d013b4afab3a9fd4dc44bfd8a3d04fb0c
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2024-07-05 20:40:23 +0200

    Adjusted card component and dashboard

--- ecommerce_mock_project/frontend/admin-dash/src/app/pages/page-dashboard/page-dashboard.component.ts
+++ ecommerce_mock_project/frontend/admin-dash/src/app/pages/page-dashboard/page-dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-dashboard',
   standalone: true,
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, RouterLink],
   templateUrl: './page-dashboard.component.html',
   styleUrl: './page-dashboard.component.css',
 })
-export class PageDashboardComponent {}
+export class PageDashboardComponent {
+  totalOrders: number = 10000;
+  totalCustomers: number = 3500;
+}
